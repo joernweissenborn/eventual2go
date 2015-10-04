7fde7ebb995c396da03090edaffbace862ff7928
joernweissenborn <joern.weissenborn@gmail.com>
2015-10-04 17:48:45 +0200

    Created Collector

---
+++ collector.go
@@ -0,0 +1,91 @@
+package eventual2go
+
+type Collector struct {
+	r      *Reactor
+	pile   []Data
+	remove *Future
+}
+
+func NewCollector() (c *Collector) {
+
+	c = &Collector{
+		NewReactor(),
+		[]Data{},
+		NewFuture(),
+	}
+
+	c.r.React("add", c.collect)
+	c.r.React("get", c.get)
+	c.r.React("empty", c.empty)
+
+	return
+}
+
+func (c *Collector) Add(d Data) {
+	c.r.Fire("add", d)
+}
+
+func (c *Collector) AddStream(s *Stream) {
+	s.Listen(c.Add)
+}
+
+func (c *Collector) AddFuture(f *Future) {
+	f.Then(c.collectFuture)
+}
+
+func (c *Collector) collectFuture(d Data) Data {
+	c.Add(d)
+	return nil
+}
+
+func (c *Collector) AddFutureError(f *Future) {
+	f.Err(c.collectFutureErr)
+}
+
+func (c *Collector) collectFutureErr(e error) (Data, error) {
+	c.Add(e)
+	return nil, nil
+}
+
+func (c *Collector) Get() (d Data) {
+	cd := make(chan Data)
+	defer close(cd)
+	c.r.Fire("get", cd)
+	d = <-cd
+	return
+}
+
+func (c *Collector) Empty() (e bool) {
+	cd := make(chan bool)
+	defer close(cd)
+	c.r.Fire("empty", cd)
+	e = <-cd
+	return
+}
+
+func (c *Collector) collect(d Data) {
+
+	c.pile = append(c.pile, d)
+
+}
+
+func (c *Collector) get(d Data) {
+
+	cd := d.(chan Data)
+
+	if len(c.pile) != 0 {
+		cd <- c.pile[0]
+		c.pile = c.pile[1:]
+	} else {
+		cd <- nil
+	}
+
+}
+
+func (c *Collector) empty(d Data) {
+
+	cd := d.(chan bool)
+
+	cd <- len(c.pile) == 0
+
+}
---
+++ collector_test.go
@@ -0,0 +1,57 @@
+package eventual2go
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCollector(t *testing.T) {
+
+	c := NewCollector()
+
+	if !c.Empty() {
+		t.Error("Collector is not empty after init")
+	}
+
+	c.Add("bla")
+
+	//Add is non-blocking async
+	time.Sleep(1 * time.Millisecond)
+
+	if c.Empty() {
+		t.Error("Collector is still empty after add")
+	}
+
+	d, ok := c.Get().(string)
+
+	if !ok {
+		t.Error("Wrong Data Type", d)
+	}
+	if d != "bla" {
+		t.Error("Wrong data", d)
+	}
+
+	if !c.Empty() {
+		t.Error("Collector is not empty after get")
+	}
+
+	f := NewCompleter()
+	c.AddFuture(f.Future())
+	f.Complete("bla")
+
+	//Add is non-blocking async
+	time.Sleep(1 * time.Millisecond)
+
+	if c.Empty() {
+		t.Error("Collector is still empty after add")
+	}
+
+	d, ok = c.Get().(string)
+
+	if !ok {
+		t.Error("Wrong Data Type", d)
+	}
+	if d != "bla" {
+		t.Error("Wrong data", d)
+	}
+}
