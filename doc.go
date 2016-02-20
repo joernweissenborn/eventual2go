45177de75301d2e5521eb8bcfc4c5185f861f825
Joern Weissenborn <joern.weissenborn@gmail.com>
2016-02-20 21:45:53 +0100

    updated doc

--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 /*
 Package eventual2go is a library for event driven development. It provides implementations for futures and streams, as can be found in many modern languages.
+
+eventual2go deals with cases, where you are in event-driven enviroment and want to react to something from which you don't now when its gonna happen.
+
+Events can either be unique or not. For example, the result of reading in a large file is unique event, whereas a GET request on a webserver is not. eventual2go provides Futures for the former, Streams for the latter case.
 */
 package eventual2go
